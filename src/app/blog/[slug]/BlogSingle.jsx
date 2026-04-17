c55e7503715d80a578c0abb510a0bf690152aeb0
Mukesh Kumar <mk8701952@gmail.com>
2026-04-17 10:47:03 +0530

    kkllkl

--- src/app/blog/[slug]/BlogSingle.jsx
+++ src/app/blog/[slug]/BlogSingle.jsx
@@ -24,6 +24,7 @@ export default function BlogDetails({ post }) {
             <div className="w-full h-[260px] md:h-[480px] bg-white md:rounded-2xl overflow-hidden shadow-sm border border-[#56021F]/10">
               <Image
                 src={single?.HeroImg?.url}
+                unoptimized
                 alt={single?.HeroAltText || "Blog Image"}
                 width={1200}
                 height={800}
@@ -72,6 +73,7 @@ export default function BlogDetails({ post }) {
                 <div className="w-full my-6 ">
                   <Image
                     src={section.img.url}
+                    unoptimized
                     alt={section?.img?.altText || "Blog Image"}
                     width={900}
                     height={600}
